320f2f8b45341f584665d9acfb14f4a41176233a
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2021-04-06 00:56:20 -0300

    Fix Scripts
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- Scripts/initi3.sh
+++ Scripts/initi3.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 xrandr --setprovideroutputsource modesetting NVIDIA-0
-xrandr --output eDP-1-1 --mode 1920x1080 --primary --auto
+xrandr --output eDP-1-1 --mode 1920x1080 --primary --pos 0x1080 --auto
 
-resp=$(xrandr | grep -e "HDMI-0" | cut -d" " -f2)
-
-if [ "$resp" == "connected" ]; then
-	
-	# Preferindo uma tela 1080.
-	resp=$(xrandr | grep -e "HDMI-0" -A3 | grep -e "   ")
-	if [ $(echo $resp | grep "1920x1080" | wc -l) -gt 0 ]; then
-		xrandr --output HDMI-0 --mode 1920x1080 --left-of eDP-1-1 --auto
-	fi
-
-fi
-
+# Preferindo uma tela 1080.
+# xrandr --output HDMI-0 --mode 2560x1080 --pos 0x0 --auto
+xrandr --output HDMI-0 --mode 3840x2160 --right-of eDP-1-1 --auto --primary
+# Terceiro Monitor
+xrandr --output DVI-I-2-1 --mode 1280x1024 --pos 1920x1080 --auto
 
 xrandr --auto
+
+~/.Scripts/background.sh
+~/.config/polybar/launch.sh
